37899590f66d6254e78bac511404843e0362b24e
Anna <townsendanna15@gmail.com>
2019-11-15 23:54:38 -0500

    finished closures, going to create new branch to review again quickly

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,28 +55,42 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
-console.log(fullName);
+runners.forEach(runner =>(runner.full_name = `${runner.first_name}${runner.last_name}`))
+let fullNames = runners.map(runner => runner ['full_name'])
+console.log(fullNames);
+
+
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
-console.log(allCaps); 
+let firstNameAllCaps = runners.map( runner => runner['first_name'].toUpperCase())
+console.log(firstNameAllCaps)
+
+
+
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+let runnerlargeSizeShirt = runners.filter ( runner => runner.shirt_size === 'L')
+console.log(runnerlargeSizeShirt)
+
+
+
+
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = 0;
+let donations = runners.map(runner => runner['donation']);
+let ticketPriceTotal = donations.reduce((total, donation) => total + donation);
 console.log(ticketPriceTotal);
-
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Samsumg changed their domain and now we need to correct all the emails for @samsung.com address to @sungsam.com
+runners.forEach(runner => (runner.email = runner.email.replace('@samsung.com', '@sungsam.com')));
+console.log(runners.filter(runner => runner.email.includes('sungsam')));
 
 // Problem 2
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+// that manipulates variables defined in the outer scope.
+// The outer scope can be a parent function, or the top level of the script.
+
+const closure = () => {
+  let closureData = '';
+  return data => (closureData += data);
+};
+
+const mytestClosure = closure();
+console.log(mytestClosure('Hi!'));
+console.log(mytestClosure(" ...here we are again hi"));
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -7,6 +18,10 @@
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 1;
+  return () => {
+    return count++;
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
@@ -14,8 +29,25 @@ const counter = () => {
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFunction = (limit = 10) => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: () => {
+      count = count >= limit ? 1 : count + 1;
+      return count;
+    },
+    decrement: () => {
+      count = count >= limit ? 0 : count - 1;
+      return count;
+    },
+  };
 };
+
+console.log('Counter V3');
+const counterV3 = counterFunction();
+console.log(`This should be 1: ${counterV3.increment()}`);
+console.log(`This should be 2: ${counterV3.increment()}`);
+console.log(`This should be 1: ${counterV3.decrement()}`);
